cecb3be8ed7d604a8e7c31593b55a34ca3150dbc
배단비 <baedanbi@baedanbis-MacBook-Air.local>
2024-03-17 17:51:45 +0900

    Create: fist my lazy git
    Hello

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+    end,
+  },
+}
