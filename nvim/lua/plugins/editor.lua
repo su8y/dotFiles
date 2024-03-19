c139a879ec18a02bd7cc55e523b0c153c2c04205
suby <bsa0530@gmail.com>
2024-03-19 21:23:20 +0900

    set:ADD  colorschme solarized-osaka & Change keymapping

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -82,8 +82,7 @@ return {
             additional_args = { "--hidden" },
           })
         end,
-        desc =
-        "Search for a string in your current working directory and get results live as you type, respects .gitignore",
+        desc = "Search for a string in your current working directory and get results live as you type, respects .gitignore",
       },
       {
         "\\\\",
@@ -126,7 +125,7 @@ return {
         desc = "Lists Function names, variables, from Treesitter",
       },
       {
-        "sf",
+        "<leader>ff",
         function()
           local telescope = require("telescope")
 
